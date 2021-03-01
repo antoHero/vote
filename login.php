5166bea91f3c470c902db3534b3c8ad7ad6ccb1e
antoHero <veeqanto@gmail.com>
2021-03-01 13:43:39 +0100

    corrected credentials

--- login.php
+++ login.php
@@ -56,11 +56,11 @@
            $mail->isSMTP();                                       // Set mailer to use SMTP  
            $mail->Host = 'smtp.gmail.com;';                       // Specify main and backup SMTP servers  
            $mail->SMTPAuth = true;                                // Enable SMTP authentication  
-           $mail->Username = 'veeqanto@gmail.com';               // your SMTP username  
-           $mail->Password = 'antoakoke';                      // your SMTP password  
+           $mail->Username = 'example@gmail.com';               // your SMTP username  
+           $mail->Password = 'your gmail password';                      // your SMTP password  
            $mail->SMTPSecure = 'tls';                             // Enable TLS encryption, `ssl` also accepted  
            $mail->Port = 587;                                     // TCP port to connect to  
-           $mail->setFrom('antoakay@gmail.com', 'Akoke Anto');  
+           $mail->setFrom('example@gmail.com', 'CSC VOTE');  
            $mail->addAddress($email);                 // Name is optional  
            // $mail->addReplyTo('info@example.com', 'Information');  
            // $mail->addCC('cc@example.com');                        // set your CC email address  
